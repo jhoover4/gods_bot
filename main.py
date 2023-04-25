5f49d95eb67c4f706c323348b2041748473f6a4a
jhoover4 <jordan@hoovermld.com>
2023-04-24 22:51:10 -0500

    Fix issues and add todos.

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
+import os
+
 import discord
 import sqlite3
 import datetime
 from discord.ext import tasks
+from dotenv import load_dotenv
 
-TOKEN = "your_bot_token_here"
+load_dotenv()
+
+ONE_WEEK = 604800  # 604800 seconds = 1 week
 DATABASE = "meetings.db"
+TOKEN = os.getenv("BOT_TOKEN")
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.message_content = True
 
-# Connect to the SQLite database
+client = discord.Client(intents=intents)
+
 conn = sqlite3.connect(DATABASE)
 
-# Create a table to store meeting dates
 conn.execute(
     """CREATE TABLE IF NOT EXISTS meetings
              (id INTEGER PRIMARY KEY,
@@ -22,71 +30,72 @@ conn.execute(
 @client.event
 async def on_ready():
     print("Logged in as {0.user}".format(client))
-    weekly_reminder.start()  # Start the weekly reminder task
+    weekly_reminder.start()
 
 
 @tasks.loop(
-    count=None, seconds=604800
-)  # Run the task every week (604800 seconds = 1 week)
+    count=None, seconds=ONE_WEEK
+)
 async def weekly_reminder():
-    # Retrieve the next meeting date from the database
     cursor = conn.cursor()
     cursor.execute("SELECT date FROM meetings ORDER BY date ASC LIMIT 1")
     result = cursor.fetchone()
     if result:
         meeting_date = datetime.datetime.strptime(result[0], "%Y-%m-%d").date()
-        # Calculate the number of days until the next meeting
         days_until = (meeting_date - datetime.date.today()).days
-        if days_until == 0:
-            response = "The next meeting is today!"
-        elif days_until == 1:
+        if days_until == 1:
             response = "The next meeting is tomorrow!"
+        elif days_until == 7:
+            response = "The next meeting is in one week!"
         else:
-            response = "The next meeting is in {} days on {}.".format(
-                days_until, meeting_date.strftime("%A, %B %d")
-            )
+            response = "There are no meetings scheduled."
     else:
         response = "There are no meetings scheduled."
-    # Send the reminder message to the channel
-    channel = client.get_channel(YOUR_CHANNEL_ID_HERE)
+    channel = client.get_channel(CHANNEL_ID)
     await channel.send(response)
 
 
 @client.event
 async def on_message(message):
     if message.content.startswith("!schedule"):
-        # Retrieve the next meeting date from the database
-        cursor = conn.cursor()
-        cursor.execute("SELECT date FROM meetings ORDER BY date ASC LIMIT 1")
-        result = cursor.fetchone()
-        if result:
-            meeting_date = datetime.datetime.strptime(result[0], "%Y-%m-%d").date()
-            # Calculate the number of days until the next meeting
-            days_until = (meeting_date - datetime.date.today()).days
-            if days_until == 0:
-                response = "The next meeting is today!"
-            elif days_until == 1:
-                response = "The next meeting is tomorrow!"
-            else:
-                response = "The next meeting is in {} days on {}.".format(
-                    days_until, meeting_date.strftime("%A, %B %d")
-                )
-        else:
-            response = "There are no meetings scheduled."
-        await message.channel.send(response)
-
+        await message.channel.send(get_all_meetings())
     elif message.content.startswith("!addmeeting"):
-        # Extract the date from the message and insert it into the database
-        try:
-            date_string = message.content.split(" ")[1]
-            meeting_date = datetime.datetime.strptime(date_string, "%Y-%m-%d").date()
-            conn.execute("INSERT INTO meetings (date) VALUES (?)", (date_string,))
-            conn.commit()
-            response = "Meeting scheduled for {}.".format(
-                meeting_date.strftime("%A, %B %d")
-            )
-        except (IndexError, ValueError):
-            response = "Invalid date format. Please use YYYY-MM-DD."
-        await message
+        date_str = message.content.split(" ")[1]
+        return add_meeting(date_str)
+    else:
+        return "I don't have any more commands. Pray to god that I have the strength to find more."
 
-        return response
+
+def get_all_meetings():
+    cursor = conn.cursor()
+    cursor.execute("SELECT date FROM meetings ORDER BY date ASC")
+    result = cursor.fetchone()
+    if result:
+        # TODO: List all meetings
+        for meeting in result:
+            response += "The next meeting is in {} days on {}.".format(
+                days_until, meeting_date.strftime("%A, %B %d")
+            )
+    else:
+        response = "There are no meetings scheduled."
+
+    return response
+
+
+def add_meeting(date):
+    # TODO: Evaluate date, if bad error
+    meeting_date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
+
+    try:
+        conn.execute("INSERT INTO meetings (date) VALUES (?)", (meeting_date,))
+        conn.commit()
+        response = "Meeting scheduled for {}.".format(
+            meeting_date.strftime("%A, %B %d")
+        )
+    except (IndexError, ValueError):
+        response = "Invalid date format. Please use YYYY-MM-DD."
+
+    return response
+
+
+client.run(TOKEN)
